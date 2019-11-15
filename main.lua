d09fc955b1429916e7ef07e89a3add698fc8f428
chico3434 <chicokarkow@hotmail.com>
2019-11-15 13:26:43 -0300

    Nuvem se movimenta + Fica escuro quando a nuvem cobre metade do Sol

--- main.lua
+++ main.lua
@@ -3,10 +3,31 @@ function color(r, g, b)
   return {r/255, g/255, b/255}
 end
 
+-- x da nuvem
+x = 450
+-- direcao da nuvem
+left = true
+
+function love.update(dt)
+  if x-50 < 0 then
+    left = false
+  end
+  if x+50+210 > 800 then
+    left = true
+  end
+
+  if left then
+    x = x - dt * 100
+  else 
+    x = x + dt * 100
+  end
+end
+
 function love.draw()
   -- cores
   blue = color(0,0,255)
   light_blue = color(0,191,255)
+  dark_blue = color(0,0,139)
   green = color(0,255,0)
   yellow = color(255,255,0)
   brown = color(139,69,19)
@@ -16,7 +37,12 @@ function love.draw()
   gray = color(144,144,144)
 
   -- fundo
-  love.graphics.setBackgroundColor(light_blue)
+  -- fica escuro quando a nuvem cobre pelo menos metade do sol
+  if x - 50 <= 100 then
+    love.graphics.setBackgroundColor(dark_blue)
+  else
+    love.graphics.setBackgroundColor(light_blue)
+  end
 
   -- grama
   love.graphics.setColor(green)
@@ -63,9 +89,9 @@ function love.draw()
 
   -- nuvem
   love.graphics.setColor(white)
-  love.graphics.circle("fill", 450, 100, 50, 50)
-  love.graphics.circle("fill", 520, 100, 50, 50)
-  love.graphics.circle("fill", 590, 100, 50, 50)
-  love.graphics.circle("fill", 660, 100, 50, 50)
+  love.graphics.circle("fill", x, 100, 50, 50)
+  love.graphics.circle("fill", x + 70, 100, 50, 50)
+  love.graphics.circle("fill", x + 140, 100, 50, 50)
+  love.graphics.circle("fill", x + 210, 100, 50, 50)
 
 end
